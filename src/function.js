3d7c1a8dd7eef8dc51d6045a54f9981fbaf3f3e3
Natzkg <108194075+Natzkg@users.noreply.github.com>
2022-10-03 19:59:51 -0400

    updated Vanilla Weather app project

--- src/function.js
+++ src/function.js
@@ -8,6 +8,7 @@ function formatDate(timestamp) {
     if (minutes < 10) {
       minutes = `0${minutes}`;
     }
+  
     let days = [
       "Sunday",
       "Monday",
@@ -20,7 +21,6 @@ function formatDate(timestamp) {
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
   }
-
   
   function formatDay(timestamp) {
     let date = new Date(timestamp * 1000);
@@ -29,7 +29,6 @@ function formatDate(timestamp) {
   
     return days[day];
   }
-
   
   function displayForecast(response) {
     let forecast = response.data.daily;
@@ -37,33 +36,29 @@ function formatDate(timestamp) {
     let forecastElement = document.querySelector("#forecast");
   
     let forecastHTML = `<div class="row">`;
-  
     forecast.forEach(function (forecastDay, index) {
       if (index < 6) {
         forecastHTML =
           forecastHTML +
           `
-      <div class="col-2">
-        <div class="forecast-box shadow-sm p-4 mb-3 mt-3 bg-body rounded ">
+        <div class="col-2 mb-4 mt-4">
           <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
           <img
             src="http://openweathermap.org/img/wn/${
               forecastDay.weather[0].icon
             }@2x.png"
             alt=""
-            width="50"
+            width="42"
           />
           <div class="weather-forecast-temperatures">
             <span class="weather-forecast-temperature-max"> ${Math.round(
               forecastDay.temp.max
             )}° </span>
-            <span class="class"></span>
             <span class="weather-forecast-temperature-min"> ${Math.round(
               forecastDay.temp.min
             )}° </span>
           </div>
         </div>
-      </div>
     `;
       }
     });
@@ -73,10 +68,8 @@ function formatDate(timestamp) {
   }
   
   function getForecast(coordinates) {
-    console.log(coordinates);
     let apiKey = "3f6be1c407b0d9d1933561808db358ba";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-    console.log(apiUrl);
     axios.get(apiUrl).then(displayForecast);
   }
   
@@ -86,6 +79,8 @@ function formatDate(timestamp) {
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let pressureElement = document.querySelector("#pressure");
+    let feelsElement = document.querySelector("#feels");
     let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon");
   
@@ -96,6 +91,8 @@ function formatDate(timestamp) {
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    pressureElement.innerHTML = response.data.main.pressure;
+    feelsElement.innerHTML = Math.round(response.data.main.feels_like);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
     iconElement.setAttribute(
       "src",
@@ -108,7 +105,6 @@ function formatDate(timestamp) {
   
   function search(city) {
     let apiKey = "3f6be1c407b0d9d1933561808db358ba";
-  
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayTemperature);
   }
@@ -119,50 +115,7 @@ function formatDate(timestamp) {
     search(cityInputElement.value);
   }
   
-  function displayFahrenheitTemperature(event) {
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    celsiusLink.classList.remove("active");
-    fahrenheitLink.classList.add("active");
-    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  
-    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-  }
-  
-  function displayCelsiusTemperature(event) {
-    event.preventDefault();
-    celsiusLink.classList.add("active");
-    fahrenheitLink.classList.remove("active");
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(celsiusTemperature);
-  }
-  
-  let celsiusTemperature = null;
-  
   let form = document.querySelector("#search-form");
   form.addEventListener("submit", handleSubmit);
   
-  let fahrenheitLink = document.querySelector("#fahrenheit-link");
-  fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-  
-  let celsiusLink = document.querySelector("#celsius-link");
-  celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
-
-  function searchLocation(position) {
-    let apiKey = "3f6be1c407b0d9d1933561808db358ba";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  
-    axios.get(apiUrl).then(displayWeatherCondition);
-  }
-  
-  function getCurrentLocation(event) {
-    event.preventDefault();
-    navigator.geolocation.getCurrentPosition(searchLocation);
-  }
-  
-  let currentLocationButton = document.querySelector("#current-location-button");
-  currentLocationButton.addEventListener("click", getCurrentLocation);
-  
-
-  search("Cupertino");
+  search("Douala");
